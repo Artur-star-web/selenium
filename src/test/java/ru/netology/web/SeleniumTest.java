e0b7c7e5be03f1367bc628cb27d0f683b265f4fc
serzh test <Artur1993xxx@yandex.ru>
2025-04-13 18:59:45 +0300

    Отправка заявки с валидными данными (Доработка1)

--- src/test/java/ru/netology/web/SeleniumTest.java
+++ src/test/java/ru/netology/web/SeleniumTest.java
@@ -15,6 +15,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class SeleniumTest {
@@ -44,13 +45,13 @@ public class SeleniumTest {
     @Test
     void shouldTestV1() {
         List<WebElement> elements = driver.findElements(By.className("input__control"));
-        elements.get(0).sendKeys("Василий");
-        elements.get(1).sendKeys("+79270000000");
-        driver.findElement(By.className("checkbox__box")).click();
-        driver.findElement(By.className("button")).click();
-        String text = driver.findElement(By.cssSelector(".App_appContainer__3jRx1")).getText();
-        assertEquals("Заявка на дебетовую карту\n" +
-                "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
+        driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Василий");
+        driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79270000000");
+        driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        WebElement resultElement = driver.findElement(By.cssSelector("[data-test-id='order-success']"));
+        assertTrue(resultElement.isDisplayed());
+        assertEquals("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", resultElement.getText().trim());
     }
 
 }
