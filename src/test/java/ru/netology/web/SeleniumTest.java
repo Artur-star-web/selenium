74132efb26e076c3af51a841b311f02a64efe952
serzh test <Artur1993xxx@yandex.ru>
2025-04-13 16:30:31 +0300

    Отправка заявки с валидными данными (ремарка)

--- src/test/java/ru/netology/web/SeleniumTest.java
+++ src/test/java/ru/netology/web/SeleniumTest.java
@@ -30,7 +30,7 @@ public class SeleniumTest {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         driver = new ChromeDriver(options);
         driver.get("http://localhost:9999");
     }
