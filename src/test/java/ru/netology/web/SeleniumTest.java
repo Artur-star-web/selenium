941f14f9e606e5b53392f9f3dd02c382210fd8aa
serzh test <Artur1993xxx@yandex.ru>
2025-04-13 16:22:33 +0300

    Отправка заявки с валидными данными

--- src/test/java/ru/netology/web/SeleniumTest.java
+++ src/test/java/ru/netology/web/SeleniumTest.java
@@ -43,8 +43,14 @@ public class SeleniumTest {
 
     @Test
     void shouldTestV1() {
-        List<WebElement> elements = driver.findElements(By.className("input class"));
-        
+        List<WebElement> elements = driver.findElements(By.className("input__control"));
+        elements.get(0).sendKeys("Василий");
+        elements.get(1).sendKeys("+79270000000");
+        driver.findElement(By.className("checkbox__box")).click();
+        driver.findElement(By.className("button")).click();
+        String text = driver.findElement(By.className("App_appContainer__3jRx1")).getText();
+        assertEquals("Заявка на дебетовую карту\n" +
+                "  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.", text.trim());
     }
 
 }
